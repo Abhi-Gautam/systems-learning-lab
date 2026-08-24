444fff6988ea81cfc2341e3ea73d4920688f2b7b
Abhishek Gautam <57491668+Abhi-Gautam@users.noreply.github.com>
2026-08-24 23:46:05 +0530

    Implement 02-lc707-design-linked-list (sentinel, ASan clean)

--- labs/cs-from-silicone/labs/01-machine-code/data-structures/02-lc707-design-linked-list/impl.cpp
+++ labs/cs-from-silicone/labs/01-machine-code/data-structures/02-lc707-design-linked-list/impl.cpp
@@ -1,5 +1,49 @@
 #include "impl.h"
-MyLinkedList::MyLinkedList():head_(nullptr),tail_(nullptr),size_(0){}
-MyLinkedList::~MyLinkedList(){/* TODO */} int MyLinkedList::get(int){return -1;}
-void MyLinkedList::addAtHead(int){/* TODO*/} void MyLinkedList::addAtTail(int){/* TODO*/}
-void MyLinkedList::addAtIndex(int,int){/* TODO*/} void MyLinkedList::deleteAtIndex(int){/* TODO*/}
+
+MyLinkedList::MyLinkedList() : dummy_(new Node()), size_(0) {
+    dummy_->next = dummy_;
+}
+
+MyLinkedList::~MyLinkedList() { clear(); delete dummy_; }
+
+void MyLinkedList::clear() {
+    Node *cur = dummy_->next;
+    while (cur != dummy_) {
+        Node *nxt = cur->next;
+        delete cur;
+        cur = nxt;
+    }
+    dummy_->next = dummy_;
+    size_ = 0;
+}
+
+MyLinkedList::Node *MyLinkedList::prev_of(int index) {
+    Node *prev = dummy_;
+    for (int i = 0; i < index; ++i) prev = prev->next;
+    return prev;
+}
+
+int MyLinkedList::get(int index) {
+    if (index < 0 || index >= size_) return -1;
+    return prev_of(index)->next->val;
+}
+
+void MyLinkedList::addAtHead(int val) { addAtIndex(0, val); }
+
+void MyLinkedList::addAtTail(int val) { addAtIndex(size_, val); }
+
+void MyLinkedList::addAtIndex(int index, int val) {
+    if (index < 0 || index > size_) return;
+    Node *prev = prev_of(index);
+    prev->next = new Node(val, prev->next);
+    ++size_;
+}
+
+void MyLinkedList::deleteAtIndex(int index) {
+    if (index < 0 || index >= size_) return;
+    Node *prev = prev_of(index);
+    Node *dead = prev->next;
+    prev->next = dead->next;
+    delete dead;
+    --size_;
+}
--- labs/cs-from-silicone/labs/01-machine-code/data-structures/02-lc707-design-linked-list/impl.h
+++ labs/cs-from-silicone/labs/01-machine-code/data-structures/02-lc707-design-linked-list/impl.h
@@ -1,8 +1,24 @@
 #pragma once
 class MyLinkedList {
-public: MyLinkedList(); ~MyLinkedList();
-    MyLinkedList(const MyLinkedList&) = delete; MyLinkedList& operator=(const MyLinkedList&) = delete;
-    int get(int index); void addAtHead(int val); void addAtTail(int val);
-    void addAtIndex(int index,int val); void deleteAtIndex(int index);
-private: struct Node{int val;Node*next;explicit Node(int v,Node*n=nullptr):val(v),next(n){}}; Node*head_; Node*tail_; int size_;
+  public:
+    MyLinkedList();
+    ~MyLinkedList();
+    MyLinkedList(const MyLinkedList &) = delete;
+    MyLinkedList &operator=(const MyLinkedList &) = delete;
+    int get(int index);
+    void addAtHead(int val);
+    void addAtTail(int val);
+    void addAtIndex(int index, int val);
+    void deleteAtIndex(int index);
+
+  private:
+    struct Node {
+        int val;
+        Node *next;
+        explicit Node(int v = 0, Node *n = nullptr) : val(v), next(n) {}
+    };
+    void clear();
+    Node *prev_of(int index);
+    Node *dummy_;
+    int size_;
 };
